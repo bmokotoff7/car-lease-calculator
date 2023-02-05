89f83b9fd95dbe7c9d6b7e793c0c2c566850dd9e
bmokotoff7 <116100468+bmokotoff7@users.noreply.github.com>
2023-02-05 15:37:43 -0500

    File Creation/Planning
    Created the following files: index.html, script.js, styles.css. Wrote pseudocode in script.js and index.html to describe basic functionality and site layout.

---
+++ script.js
@@ -0,0 +1,21 @@
+// create car object to hold all car/price related fields
+
+// create function to calculate adjusted capitalized cost (takes car object as a field)
+
+// create function to convert money factor to an equivalent interest rate
+
+// create a function to calculate the total depreciation
+
+// create a function to calculate the estimated buyout price
+
+// create a function to calculate the monthly depreciation
+
+// create a function to calculate the total rent charge
+
+// create a function to calculate the monthly rent charge
+
+// create a function to calculate the monthly payment
+
+// create a function to calculate total of monthly payments
+
+// create a function to calculate total cost (total cost of monthly payments + down payment)
