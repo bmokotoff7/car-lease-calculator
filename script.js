137f5eba025016c94681b99d743c0d16fd841a4c
bmokotoff7 <116100468+bmokotoff7@users.noreply.github.com>
2023-02-05 16:11:39 -0500

    Update script.js
    Developed the Car and LeaseInfo classes with constructors to create the Car and LeaseInfo objects.

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
-// create car object to hold all car/price related fields
+class Car {
+    /**
+     * Constructs a new Car object
+     * @param year - model year of Car
+     * @param make - Car maker/manufacturer/brand
+     * @param model - Car model name
+     * @param trim - trim level of Car
+     */
+    constructor(year, make, model, trim) {
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.trim = trim;
+    }
+}
+
+class LeaseInfo {
+    /**
+     * Constructs a new LeaseInfo object to hold all information related to a car lease calculation
+     * @param car - a new or existing Car object
+     * @param msrp - Manufacturer Suggested Retail Price of the car
+     * @param netCapCost - the net capitalized cost of the car (negotiated selling price)
+     * @param downPayment - the total down payment made on the car
+     * @param residualValue - the value of the car at the end of the lease term; represented as a percentage
+     * @param moneyFactor - the interest rate on the lease
+     * @param leaseTerm - the number of months in the lease
+     */
+    constructor(car, msrp, netCapCost, downPayment, residualValue, moneyFactor, leaseTerm) {
+        this.car = car;
+        this.msrp = msrp;
+        this.netCapCost = netCapCost;
+        this.downPayment = downPayment;
+        this.residualValue = residualValue;
+        this.moneyFactor = moneyFactor;
+        this.leaseTerm = leaseTerm;
+    }
+}
 
 // create function to calculate adjusted capitalized cost (takes car object as a field)
 
